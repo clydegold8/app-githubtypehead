bcfd036273857253904355c2e33875c9794c7837
ClydeIan <ian.clyde24@gmail.com>
2021-07-22 12:19:35 +0800

    Added components for typeheads

---
+++ src/components/table-typeheads.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typeheads from './typeheads';
+
+function TableTypeheads(){
+
+    return(
+        <div className="container">
+            <Typeheads></Typeheads>
+        </div>
+    );
+}
+
+export default TableTypeheads;
---
+++ src/components/typeheads.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './css/table-typeheads.css';
+
+function Typeheads(){
+
+    return(
+        <div className="flex-container">
+            <div className="flex-item">
+                One
+            </div>
+            <div className="flex-item">
+                Two
+            </div>
+            <div className="flex-item">
+                Three
+            </div>
+        </div>
+    );
+}
+
+export default Typeheads;
